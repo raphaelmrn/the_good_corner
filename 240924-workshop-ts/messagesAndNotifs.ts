9eb4e9c62d2cf6c5a26176954e24b9da1a419b26
LoicBrassart <loic.brassart@gmail.com>
2024-09-24 12:02:00 +0200

    Ex 03

--- 240924-workshop-ts/messagesAndNotifs.ts
+++ 240924-workshop-ts/messagesAndNotifs.ts
@@ -4,7 +4,11 @@ type Message = {
 	isRead?: boolean;
 };
 type Thread = Message[];
-//type Thread = Array<Message>
+// type Thread = Array<Message>
+type Notif = {
+	at: number;
+	state: "read" | "unread";
+};
 
 const messsageText = "Hello Bob!";
 const messageCreatedAt = new Date();
@@ -22,10 +26,13 @@ const message2: Message = { from: "Alice", message: "Hi Bob!" };
 
 const messages: Thread = [message1, message2];
 function readMessages(messages: Thread): number {
-	// for(let i=0;i<messages.length;i++) {
-	//     messages[i].isRead=true
+	// for (let i = 0; i < messages.length; i++) {
+	// 	messages[i].isRead = true;
 	// }
 	messages = messages.map((msg) => ({ ...msg, isRead: true }));
 	return messages.length;
 }
 readMessages(messages);
+
+const notification1: Notif = { at: 1694011133, state: "unread" };
+const notification2: Notif = { at: 1694011532, state: "read" };
