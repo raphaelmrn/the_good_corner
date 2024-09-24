4943edc306e83f6411fe69c48438942fcd6041fb
LoicBrassart <loic.brassart@gmail.com>
2024-09-24 12:00:18 +0200

    Ex 01

--- 240924-workshop-ts/messagesAndNotifs.ts
+++ 240924-workshop-ts/messagesAndNotifs.ts
@@ -1,6 +1,19 @@
+type Message = {
+	from: string;
+	message: string;
+	isRead?: boolean;
+};
+
 const messsageText = "Hello Bob!";
 const messageCreatedAt = new Date();
 
 function stringifyMessage(text: string, createdAt: Date) {
 	return `${text}, the ${createdAt.toLocaleDateString()}`;
 }
+
+const message1: Message = {
+	from: "Bob",
+	message: "Hello Alice!",
+	isRead: true,
+};
+const message2: Message = { from: "Alice", message: "Hi Bob!" };
