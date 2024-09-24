c679bc0cddd7431b7fcd78f7be936854ee728013
LoicBrassart <loic.brassart@gmail.com>
2024-09-24 12:01:19 +0200

    Ex 02

--- 240924-workshop-ts/messagesAndNotifs.ts
+++ 240924-workshop-ts/messagesAndNotifs.ts
@@ -3,6 +3,8 @@ type Message = {
 	message: string;
 	isRead?: boolean;
 };
+type Thread = Message[];
+//type Thread = Array<Message>
 
 const messsageText = "Hello Bob!";
 const messageCreatedAt = new Date();
@@ -17,3 +19,13 @@ const message1: Message = {
 	isRead: true,
 };
 const message2: Message = { from: "Alice", message: "Hi Bob!" };
+
+const messages: Thread = [message1, message2];
+function readMessages(messages: Thread): number {
+	// for(let i=0;i<messages.length;i++) {
+	//     messages[i].isRead=true
+	// }
+	messages = messages.map((msg) => ({ ...msg, isRead: true }));
+	return messages.length;
+}
+readMessages(messages);
