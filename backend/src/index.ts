9322051ec0257107259fe897a1eabd60e936acff
raphaelmrn <rmarion@mobilygreen.fr>
2024-09-25 16:53:46 +0200

    create new table and add first row

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,21 @@
 import express from "express";
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database("./the-good-corner.sqlite");
 
 const app = express();
 const port = 4000;
+
+type Ad = {
+  id: number;
+  title: string;
+  description: string;
+  owner: string;
+  price: number;
+  picture: string;
+  location: string;
+  createdAt: string;
+};
 
 app.use(express.json());
 
@@ -10,41 +24,25 @@ app.get("/", (req, res) => {
 });
 
 app.get("/ads", (req, res) => {
-  res.json(ads);
+  db.all("SELECT $ FROM ad", (err, rows) => {
+    res.json(rows);
+  });
 });
 
 app.post("/ads", (req, res) => {
-  ads.push(req.body);
-  res.send("Request received, check the backend terminal");
+  const { title, description, owner, price, createdAt, picture, location } =
+    req.body;
+  db.run(
+    "INSERT INTO ad ('title', 'description', 'owner', 'price', 'createdAt', 'picture', 'location') values (?, ?, ?, ?, ?, ?, ?)",
+    [title, description, owner, price, createdAt, picture, location],
+    (err) => {
+      console.log(err);
+      res.send("Request received, check the backend terminal");
+    }
+  );
+  // ads.push(req.body);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-const ads = [
-  {
-    id: 1,
-    title: "Bike to sell",
-    description:
-      "My bike is blue, working fine. I'm selling it because I've got a new one",
-    owner: "bike.seller@gmail.com",
-    price: 100,
-    picture:
-      "https://images.lecho.be/view?iid=dc:113129565&context=ONLINE&ratio=16/9&width=640&u=1508242455000",
-    location: "Paris",
-    createdAt: "2023-09-05T10:13:14.755Z",
-  },
-  {
-    id: 2,
-    title: "Car to sell",
-    description:
-      "My car is blue, working fine. I'm selling it because I've got a new one",
-    owner: "car.seller@gmail.com",
-    price: 10000,
-    picture:
-      "https://www.automobile-magazine.fr/asset/cms/34973/config/28294/apres-plusieurs-prototypes-la-bollore-bluecar-a-fini-par-devoiler-sa-version-definitive.jpg",
-    location: "Paris",
-    createdAt: "2023-10-05T10:14:15.922Z",
-  },
-];
